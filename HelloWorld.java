ab776a741f481c9ddf7643b89d3ebadea7948ceb
nakulmaheshwari <62350319+nakulmaheshwari@users.noreply.github.com>
2025-08-29 01:02:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-adpublic class HelloWorld {
+public class HelloWorld {
     public static void main(String[] args) {
         // Print message to console
         System.out.println("Hello, Wodfadfadvadvarld!");
